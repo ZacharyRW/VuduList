132ed3a4c8a388fa4dad5539e438041d29f34ae4
ZacharyRW <zachary.r.williams@gmail.com>
2017-12-20 18:55:38 -0600

    Master Update
    Working Code - will open chrome, login to vudu, go to my movies, scrape movie names, delete duplicate names, sort alphabetically, and then write to a csv file

--- vudu.py
+++ vudu.py
@@ -1,16 +1,11 @@
 #Make sure to replace USERNAME and PASSWORD with your own username and password
 
 #Import libraries
-from bs4 import BeautifulSoup
-from lxml import html
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import csv
-import json
-import re
-import requests
 import time
-import urllib.request
 
 #Login Information
 USERNAME = "example@gmail.com"
@@ -21,14 +16,14 @@ login_url = "https://my.vudu.com/MyLogin.html?type=sign_in&url=https%3A%2F%2Fwww
 url = "https://www.vudu.com/movies/#my_vudu/my_movies"
 
 def main():
-	session_requests = requests.session()
-
+	#Open Browser
 	chromedriver = 'C:\\chromedriver.exe'
 	browser = webdriver.Chrome(chromedriver)
-	browser.get('https://my.vudu.com/MyLogin.html?type=sign_in&url=https%3A%2F%2Fwww.vudu.com%2F')
+	browser.get(login_url)
 
 	time.sleep(10)
 	
+	#Login
 	username = browser.find_element_by_name('email')
 	password = browser.find_element_by_name('password')
 	
@@ -37,13 +32,58 @@ def main():
 	
 	browser.find_element_by_css_selector('.custom-button').click()
 	
-	html = urllib.request.urlopen(url)
+	time.sleep(5)
 	
-	soup = BeautifulSoup(html, 'html.parser')
+	browser.get(url)
 	
-	name_box = soup.find('div', attrs={'class': 'gwt-Label title'})
+	#If the above line doesn't work, use the one below (Included to plan for the future)
+	#browser.find_element_by_xpath("""//*[@id="widgetContainer"]/div/div/div[1]/div/div[5]/div[2]/div[1]""").click()
 	
-	print (name_box)
+	time.sleep(5)
 
+	movielist = []
+	print(movielist)
+	
+	counter = 1
+	
+	#Get movie names
+	for _ in range(1,22):
+	#for _ in range(1,3):
+		all_images = browser.find_elements_by_css_selector('.border .gwt-Image')
+		#for image in all_images[:5]: # first five elements
+		for image in all_images:
+			#print('image:', image.get_attribute('src'))
+			#print('alt:', image.get_attribute('alt'))
+			movienames = image.get_attribute('alt')
+			
+			#Add movie name to list
+			movielist.append(movienames)
+		
+			print(counter,"Scraping Done")
+			counter += 1
+		
+		#Scroll down the page to load more dynamic content
+		for _ in range(1,25):
+			ActionChains(browser).send_keys(Keys.PAGE_DOWN).perform()
+		
+		time.sleep(5)
+	
+	#Sort movie name list and delete duplicates
+	movielist = list(set(movielist))
+	movielist.sort()
+	
+	print(movielist)
+	#Write movie names to a csv file
+	file = "Example2.csv"
+	with open(file, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile, dialect='excel')
+		#writer.writerows([movielist])
+		
+		movielist2 = []
+		for movie in movielist:
+			movielist2.append([movie])
+		writer.writerows(movielist2)
+	print("Writing complete")
+	
 if __name__ == '__main__':
 	main()
